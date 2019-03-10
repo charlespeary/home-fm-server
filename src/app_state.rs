020374f89d84ad0dec9f21a2a3cf92abf31d3ffa
Sniadek <wojokarolg9@gmail.com>
2019-03-10 01:31:34 +0100

    basic downloading of song with youtube-dl via std::command

--- src/app_state.rs
+++ src/app_state.rs
@@ -1,12 +1,21 @@
-use actix_web::{HttpRequest, HttpResponse, Json, Result, State};
+use actix_web::{HttpRequest, Json, Result, State};
 use serde::ser::{SerializeStruct, Serializer};
-// to avoid name shadowing it's named as MSerialize, M stands for Makro
 use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
+use std::process::Command;
 use std::sync::{Arc, Mutex};
 
 #[derive(Clone)]
 pub struct AppState {
-	pub current_song: Arc<Mutex<String>>,
+	current_song: Arc<Mutex<String>>,
+}
+
+impl AppState {
+	pub fn new() -> Self {
+		AppState {
+			current_song: Arc::new(Mutex::new(String::new())),
+		}
+	}
 }
 
 impl Serialize for AppState {
@@ -26,8 +35,9 @@ pub fn get_app_state(req: &HttpRequest<AppState>) -> Result<Json<AppState>> {
 }
 
 #[derive(Serialize)]
-pub struct RequestResponse {
+pub struct MyResponse {
 	success: bool,
+	message: &'static str,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -36,8 +46,31 @@ pub struct Song {
 }
 
 // TODO: Make state shared across all threads
-pub fn set_current_song(song: Json<Song>, state: State<AppState>) -> Result<Json<RequestResponse>> {
+pub fn set_current_song(song: Json<Song>, state: State<AppState>) -> Result<Json<MyResponse>> {
 	*(state.current_song.lock().unwrap()) = song.name.to_string();
 	println!("current song: {}", state.current_song.lock().unwrap());
-	Ok(Json(RequestResponse { success: true }))
+	let download_result = download_song(&song.name);
+	return match download_result {
+		Ok(message) => Ok(Json(MyResponse {
+			success: true,
+			message,
+		})),
+		Err(message) => Ok(Json(MyResponse {
+			success: false,
+			message,
+		})),
+	};
+}
+
+/// Formats the sum of two numbers as string
+fn download_song(song_name: &str) -> Result<&'static str, &'static str> {
+	println!("Downloading song");
+	let static_path = PathBuf::from("static/songs");
+	let output = Command::new("youtube-dl")
+		.arg("-f bestaudio")
+		.arg(format!("-o {path}/%(title)s", path = static_path.display()))
+		.arg("https://www.youtube.com/watch?v=ZadwPbKA9Mg")
+		.output()
+		.expect("Couldn't download the song.");
+	Ok("Successfully downloaded song")
 }
--- src/system.rs
+++ src/system.rs
@@ -1,7 +1,6 @@
 use super::app_state::{get_app_state, set_current_song, AppState};
 use actix_web::{http::Method, middleware, server, App};
 use listenfd::ListenFd;
-use std::sync::{Arc, Mutex};
 
 pub struct System {}
 
@@ -9,21 +8,22 @@ impl System {
     pub fn new() -> Self {
         ::std::env::set_var("RUST_LOG", "actix_web=info");
         env_logger::init();
-        let sys = actix::System::new("ws-example");
+        let sys = actix::System::new("home-fm-server");
         // listenfd object that brings hotreloading
         let mut listenfd = ListenFd::from_env();
-        //move is necessary to give closure below ownership of counter
+
+        // Initial state of the app filled with Arc<Mutex>> to make it shareable between states
+        let state = AppState::new();
+
         let mut server = server::new(move || {
-            App::with_state(AppState {
-                current_song: Arc::new(Mutex::new(String::from("DESPACITO"))),
-            }) // <- create app with shared state
-            // add our resources (routes)
-            .resource("/song", |r| r.method(Method::GET).f(get_app_state))
-            .resource("/song/set_current", |r| {
-                r.method(Method::POST).with(set_current_song)
-            })
-            // add middleware to log stuff
-            .middleware(middleware::Logger::default())
+            App::with_state(state.clone()) // <- create app with shared state
+                // add our resources (routes)
+                .resource("/song", |r| r.method(Method::GET).f(get_app_state))
+                .resource("/song/set_current", |r| {
+                    r.method(Method::POST).with(set_current_song)
+                })
+                // add middleware to log stuff
+                .middleware(middleware::Logger::default())
         });
 
         server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
