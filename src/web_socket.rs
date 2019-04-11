e818fc65528ee165254dd8a9e380c4759776458e
Sniadek <wojokarolg9@gmail.com>
2019-04-11 14:38:07 +0200

    add heartbeat to keep connection

--- src/web_socket.rs
+++ src/web_socket.rs
@@ -14,13 +14,20 @@ pub fn ws_index(r: &HttpRequest<AppState>) -> Result<HttpResponse, Error> {
 }
 
 #[derive(Debug)]
-pub struct MyWebSocket;
+pub struct MyWebSocket {
+    hb: Instant,
+}
+
+const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(5);
+/// How long before lack of client response causes a timeout
+const CLIENT_TIMEOUT: Duration = Duration::from_secs(10);
 
 impl Actor for MyWebSocket {
     type Context = ws::WebsocketContext<Self, AppState>;
 
     fn started(&mut self, ctx: &mut Self::Context) {
         // get ClientPublisher address and send address of websocket to it
+        self.hb(ctx);
         let publisher_addr = ClientPublisher::from_registry();
         publisher_addr.do_send(RegisterWS {
             addr: ctx.address(),
@@ -40,7 +47,7 @@ const FIVE_SECONDS: Duration = Duration::from_secs(5);
 
 impl MyWebSocket {
     pub fn new() -> Self {
-        MyWebSocket {}
+        MyWebSocket { hb: Instant::now() }
     }
 
     fn send_message<T>(&self, ctx: &mut <Self as Actor>::Context, msg: &UserMessage<T>)
@@ -56,6 +63,24 @@ impl MyWebSocket {
                 eprintln!("Couldn't serialize given entity: {}", e);
             }
         }
+    }
+
+    fn hb(&self, ctx: &mut <Self as Actor>::Context) {
+        ctx.run_interval(HEARTBEAT_INTERVAL, |act, ctx| {
+            // check client heartbeats
+            if Instant::now().duration_since(act.hb) > CLIENT_TIMEOUT {
+                // heartbeat timed out
+                println!("Websocket Client heartbeat failed, disconnecting!");
+
+                // stop actor
+                ctx.stop();
+
+                // don't try to send a ping
+                return;
+            }
+
+            ctx.ping("");
+        });
     }
 }
 
@@ -89,7 +114,11 @@ impl StreamHandler<ws::Message, ws::ProtocolError> for MyWebSocket {
     fn handle(&mut self, msg: ws::Message, ctx: &mut Self::Context) {
         match msg {
             ws::Message::Ping(msg) => {
+                self.hb = Instant::now();
                 ctx.pong(&msg);
+            }
+            ws::Message::Pong(_) => {
+                self.hb = Instant::now();
             }
             ws::Message::Text(text) => {
                 let request: Request = serde_json::from_str(&text).unwrap();
@@ -125,6 +154,9 @@ impl StreamHandler<ws::Message, ws::ProtocolError> for MyWebSocket {
                     }
                 }
                 ctx.text(text)
+            }
+            ws::Message::Close(_) => {
+                ctx.stop();
             }
             ws::Message::Binary(bin) => ctx.binary(bin),
             _ => (),
