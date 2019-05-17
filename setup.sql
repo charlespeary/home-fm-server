8b22632c948f774c70704dc456d7aad6a4149231
Sniadek <wojokarolg9@gmail.com>
2019-05-17 11:44:55 +0200

    add script to setup db

---
+++ setup.sql
@@ -0,0 +1,10 @@
+CREATE TABLE songs
+(
+    id INTEGER PRIMARY KEY NOT NULL,
+    name VARCHAR NOT NULL,
+    path VARCHAR NOT NULL,
+    duration INTEGER NOT NULL,
+    thumbnail_url VARCHAR NOT NULL,
+    artists VARCHAR NOT NULL,
+    nsfw INTEGER NOT NULL DEFAULT 1
+)
---
+++ setup_db.sh
@@ -0,0 +1,2 @@
+# script to setup the db and run migration if you don't want to download diesel-cli
+sqlite3 db < setup.sql
